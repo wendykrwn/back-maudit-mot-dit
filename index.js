74293850c394ff6f5e0399056b2efdcce0693e90
Wendy <wendykrwn@gmail.com>
2026-03-24 20:29:46 +0100

    get room

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ io.on("connection", (socket) => {
 
     rooms[roomId] = {
       hostPlayerId: playerId,
-      players: [{ playerId, socketId: socket.id, pseudo }],
+      players: [{ playerId, socketId: socket.id, pseudo, score: 0 }],
       status: "waiting"
     }
 
@@ -39,6 +39,17 @@ io.on("connection", (socket) => {
     io.to(roomId).emit("room-update", rooms[roomId])
     cb(roomId)
   })
+
+    // Get room 
+    socket.on("get-room",({roomId}, cb)=>{
+        const room = rooms[roomId]
+
+        if (!room) return cb({ error: "Room not found" })
+        socket.join(roomId)
+        io.to(roomId).emit("room-update", room)
+        cb({ success: true })
+    
+    })
 
   // Rejoindre une room
   socket.on("join-room", ({ roomId, pseudo, playerId }, cb) => {
@@ -49,7 +60,7 @@ io.on("connection", (socket) => {
     if (existing) {
       existing.socketId = socket.id // reconnexion
     } else {
-      room.players.push({ playerId, socketId: socket.id, pseudo })
+      room.players.push({ playerId, socketId: socket.id, pseudo, score: 0 })
     }
 
     socket.join(roomId)
@@ -86,12 +97,32 @@ io.on("connection", (socket) => {
     io.to(roomId).emit("room-update", room)
   })
 
+  socket.on("update-score", ({ roomId, playerId, points }) => {
+    const room = rooms[roomId]
+    if (!room) return
+  
+    const player = room.players.find(p => p.playerId === playerId)
+    if (!player) return
+  
+    player.score += points
+  
+    // envoyer la room complète avec scores
+    io.to(roomId).emit("room-update", { ...room })
+  })
+  
+
   // Déconnexion
   socket.on("disconnect", () => {
     console.log("Disconnected:", socket.id)
     // Pour aller plus loin : gérer auto-removal si un joueur quitte
   })
 })
+
+app.get("/room/:id", (req, res) => {
+    const room = rooms[req.params.id]
+    if (!room) return res.status(404).json({ error: "Room not found" })
+    res.json(room)
+  })
 
 server.listen(3001, () => {
   console.log("Socket server running on http://localhost:3001")
